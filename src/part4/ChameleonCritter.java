c6bf7fbddc4109edf0f3b4520ae74da8469e99b5
Bilal Reza <rezasb@s.dcsdk12.org>
2016-02-22 10:22:33 -0700

    yay

--- src/part4/ChameleonCritter.java
+++ src/part4/ChameleonCritter.java
@@ -23,7 +23,8 @@ import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 
-import java.util.ArrayList;
+import java.awt.Color;
+import java.util.ArrayList; 
 
 /**
  * A <code>ChameleonCritter</code> takes on the color of neighboring actors as
@@ -32,18 +33,29 @@ import java.util.ArrayList;
  */
 public class ChameleonCritter extends Critter {
 	
+	private static final double DARKENING_FACTOR = 0.05;
+	
 	/**
 	 * Randomly selects a neighbor and changes this critter's color to be the
 	 * same as that neighbor's. If there are no neighbors, no action is taken.
 	 */
 	public void processActors(ArrayList<Actor> actors) {
 		int n = actors.size();
-		if (n == 0)
+		if (n == 0) {
+			darken();
 			return;
+		}
 		int r = (int) (Math.random() * n);
 
 		Actor other = actors.get(r);
 		setColor(other.getColor());
+	}
+	
+	private void darken() {
+		Color c = getColor();
+		int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+		int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+		int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
 	}
 
 	/**
