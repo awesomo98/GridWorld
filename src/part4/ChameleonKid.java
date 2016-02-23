51ca60265680948a2fcfe09a4c767dd07b154772
Bilal Reza <rezasb@s.dcsdk12.org>
2016-02-23 10:21:06 -0700

    Kid

--- src/part4/ChameleonKid.java
+++ src/part4/ChameleonKid.java
@@ -3,22 +3,32 @@ package part4;
 import java.util.ArrayList;
 
 import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 public class ChameleonKid extends ChameleonCritter {
 	public ArrayList<Actor> getActor() {
 		ArrayList<Actor> actors = new ArrayList<Actor>();
-		int[] n = {Location.AHEAD, Location.HALF_CIRCLE};
-		for (Location loc : getLocationAt(n)) {
-			Actor a = getGrid().get(loc);
-			if (a != null) {
-				actors.add(a);
+		int[] directions = {Location.AHEAD, Location.HALF_CIRCLE};
+		for (Location loc : getLocationAt()) {
+			Actor actor = getGrid().get(loc);
+			if (actor != null) {
+				actors.add(actor);
 			}
 			return actors;
 		}
 	}
-		public ArrayList<Location> getLocationAt() {
+		public ArrayList<Location> getLocationAt(int[] directions) {
+			ArrayList<Location>locs = new ArrayList<Location>();
+			Grid gr = getGrid();
+			Location loc = getLocation();
 			
+			for (int d : directions) {
+				Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+				if (gr.isValid(neighborLoc)) {
+					locs.add(neighborLoc);
+				}
+			}
 			
 		}
 }
