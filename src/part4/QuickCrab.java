c5883874fba768cb156850d11606c9243fa2a4c5
Bilal Reza <rezasb@s.dcsdk12.org>
2016-02-29 10:22:22 -0700

    End of class

--- src/part4/QuickCrab.java
+++ src/part4/QuickCrab.java
@@ -10,14 +10,31 @@ import java.util.ArrayList;
 public class QuickCrab extends CrabCritter {
 	
 	
-	public ArrayList<Location> getMoveLocations() {
-		ArrayList<Location> locs = new ArrayList<Location>();
-		int[] dirs = {Location.LEFT, Location.RIGHT};
-		for (Location loc : getLocationsInDirections(dirs)) {
-			if (getGrid().get(loc) == null) {
-				
+//	public ArrayList<Location> getMoveLocations() {
+//		ArrayList<Location> locs = new ArrayList<Location>();
+//		int[] dirs = {Location.LEFT, Location.RIGHT};
+//		for (Location loc : getLocationsInDirections(dirs)) {
+//			if (getGrid().get(loc) == null) {
+//				
+//			}
+//		}
+//		return locs;
+//	}
+		
+		public ArrayList<Location> getLocationsInDirections(int[] directions) {
+			ArrayList<Location> locs = new ArrayList<Location>();
+			Grid gr = getGrid();
+			Location loc = getLocation();
+			
+			for (int d : directions) {
+				Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+				Location secondLoc = neighborLoc.getAdjacentLocation(getDirection() + d);
+				if ((gr.isValid(neighborLoc)) && (gr.isValid(secondLoc))) {
+//					if (getGrid().get(loc) == null) {
+//						locs.add(loc);
+//					}
+				}
 			}
+			return locs;
 		}
-		return locs;
-	}
 }
