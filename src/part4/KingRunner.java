b0fdb956e7c43fd03902040056f4cd59c8005632
Bilal Reza <rezasb@s.dcsdk12.org>
2016-03-01 10:18:45 -0700

    I'm winning the commit race yaaaaaay

--- src/part4/KingRunner.java
+++ src/part4/KingRunner.java
@@ -1,10 +1,15 @@
 package part4;
 
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
+
 public class KingRunner {
 
 	public static void main(String[] args) {
-		ActorWorld world = 
-
+		ActorWorld world = new ActorWorld();
+		world.add(new Location(7,7), new KingCrab());
+		world.add(new Location(6, 5), new Rock());
 	}
 
 }
